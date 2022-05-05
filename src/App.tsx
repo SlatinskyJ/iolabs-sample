baa35edf1d014e97506ee37e5d7961f138158fd4
SlatinskyJ <jakub.slatinsky@gmail.com>
2022-05-05 19:41:21 +0200

    Project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import {createTheme, ThemeProvider} from "@mui/material";
+import {BrowserRouter} from "react-router-dom";
+
+const theme = createTheme({});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<BrowserRouter>
+				<div className="App">
+					<header className="App-header">
+						<p>Header</p>
+					</header>
+					<body className="App-body">
+						<p>Just body text.</p>
+					</body>
+				</div>
+			</BrowserRouter>
+		</ThemeProvider>
+	);
 }
 
 export default App;
